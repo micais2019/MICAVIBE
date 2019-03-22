fb7e902c603f6b6125c2a11d27556fab6482af87
Adam Bachman <abachman@mica.edu>
2019-03-22 16:26:42 -0400

    update ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+    {
+      name:   "micapulse",
+      script: "server.js",
+      // env: {
+      //   COMMON_VARIABLE: "true"
+      // },
+      env_production : {
+        NODE_ENV: "production"
+      }
+    },
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : "micaweb",
+      host : "micapulse.com",
+      ref  : "origin/master",
+      repo : "git@github.com:micais2019/micapulse.com.git",
+      path : "/var/www/app",
+      "post-deploy" : "npm install && pm2 startOrRestart ecosystem.config.js --env production"
+    }
+  }
+}
